a751b6020eed14ee3d30521df09c8613106463b6
jeremy <jeremypoirierwork@outlook.com>
2024-11-11 22:34:09 -0500

    Base des classes Entrepot/Produits (not finished)

--- 420DA3_A24_Projet/Business/Domain/Entrepot.cs
+++ 420DA3_A24_Projet/Business/Domain/Entrepot.cs
@@ -6,4 +6,26 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 internal class Entrepot {
+    // Attributs de la classe Entrepot
+    public int Id { get; set; } 
+    public string Nom { get; set; } 
+    public Adresse Adresse { get; set; } 
+    public DateTime DateCreation { get; set; } = DateTime.Now; 
+    public DateTime? DateModification { get; set; } 
+    public DateTime? DateSuppression { get; set; } 
+
+    // Constructeur par défaut
+    public Entrepot() {
+        DateCreation = DateTime.Now;
+    }
+
+    // Constructeur avec des paramètres
+    public Entrepot(string nomEntrepot, string addEntrepot, string prenomContact) {
+        nomEntrepot = this.Nom;
+        addEntrepot = this.Adresse.ToString();
+        prenomContact = prenomContact;
+        DateCreation = DateTime.Now;
+    }
 }
+
+
--- 420DA3_A24_Projet/Business/Domain/Produits.cs
+++ 420DA3_A24_Projet/Business/Domain/Produits.cs
@@ -6,4 +6,34 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 internal class Produits {
+    public int Id { get; set; } 
+    public string Nom { get; set; } 
+    public string Description { get; set; } 
+    public string CodeUPC { get; set; } 
+    public string? ImageFileName { get; set; } 
+    public Client Client { get; set; }
+    public string Fournisseur { get; set; } 
+    public string CodeProduitFournisseur { get; set; } 
+    public int QuantiteEnStock { get; set; } 
+    public int QuantiteStockVisee { get; set; } 
+    public double PoidsKg { get; set; } 
+    public DateTime DateCreation { get; set; } = DateTime.Now; 
+    public DateTime? DateModification { get; set; } 
+    public DateTime? DateSuppression { get; set; }
+
+    // Constructeur par défaut
+    public Produits() {
+        DateCreation = DateTime.Now;
+    }
+
+    // Constructeur avec des paramètres
+    public Produits(string nomProduit, string description, string codeUpc, string fournisseur, string codeProduitFournisseur) {
+        nomProduit = this.Nom;
+        description = this.Description;
+        codeUpc = this.CodeUPC;
+        fournisseur = this.Fournisseur;
+        codeProduitFournisseur = CodeProduitFournisseur;
+        //Reste des paramètres a instancier mais questions ?
+        DateCreation = DateTime.Now;
+    }
 }
