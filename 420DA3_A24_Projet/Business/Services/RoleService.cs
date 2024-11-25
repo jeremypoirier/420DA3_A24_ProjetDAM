7d70503113eb461596c6b956ca5d730fd090b1cb
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-25 09:48:07 -0500

    ajout de GetAll à RoleDAO, ajout de méthodes dans les services pour User et Role

--- 420DA3_A24_Projet/Business/Services/RoleService.cs
+++ 420DA3_A24_Projet/Business/Services/RoleService.cs
@@ -1,12 +1,90 @@
-﻿using _420DA3_A24_Projet.DataAccess.Contexts;
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
 using _420DA3_A24_Projet.DataAccess.DAOs;
 
 namespace _420DA3_A24_Projet.Business.Services;
+
+/// <summary>
+/// TODO @PROF: documenter
+/// </summary>
 internal class RoleService {
 
     private readonly RoleDAO dao;
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="parentApp"></param>
+    /// <param name="context"></param>
     public RoleService(WsysApplication parentApp, WsysDbContext context) {
         this.dao = new RoleDAO(context);
     }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public List<Role> GetAll(bool includeDeleted = false) {
+        return this.dao.GetAll(includeDeleted);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public Role? GetById(int id, bool includeDeleted = false) {
+        return this.dao.GetById(id, includeDeleted);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="roleName"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public Role? GetByRoleName(string roleName, bool includeDeleted = false) {
+        return this.dao.GetByRoleName(roleName, includeDeleted);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="criterion"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public List<Role> Search(string criterion, bool includeDeleted = false) {
+        return this.dao.Search(criterion, includeDeleted);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    public Role Create(Role role) {
+        return this.dao.Create(role);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    public Role Update(Role role) {
+        return this.dao.Update(role);
+    }
+
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <param name="softDeletes"></param>
+    /// <returns></returns>
+    public Role Delete(Role role, bool softDeletes = true) {
+        return this.dao.Delete(role, softDeletes);
+    }
+
 }
--- 420DA3_A24_Projet/Business/Services/UserService.cs
+++ 420DA3_A24_Projet/Business/Services/UserService.cs
@@ -2,6 +2,7 @@
 using _420DA3_A24_Projet.DataAccess.Contexts;
 using _420DA3_A24_Projet.DataAccess.DAOs;
 using _420DA3_A24_Projet.Presentation.Views;
+using System.Runtime.Serialization;
 
 namespace _420DA3_A24_Projet.Business.Services;
 internal class UserService {
@@ -12,6 +13,27 @@ internal class UserService {
     public UserService(WsysApplication parentApp, WsysDbContext context) {
         this.dao = new UserDAO(context);
         this.view = new UserView(parentApp);
+    }
+
+    public User? OpenUserWindowForCreation() {
+        User newUser = (User) FormatterServices.GetUninitializedObject(typeof(User));
+        DialogResult result = this.view.OpenForCreation(newUser);
+        return result == DialogResult.OK ? newUser : null;
+    }
+
+    public User? OpenUserWindowForDetailsView(User user) {
+        DialogResult result = this.view.OpenForDetailsView(user);
+        return result == DialogResult.OK ? user : null;
+    }
+
+    public User? OpenUserWindowForEdition(User user) {
+        DialogResult result = this.view.OpenForEdition(user);
+        return result == DialogResult.OK ? user : null;
+    }
+
+    public User? OpenUserWindowForDeletion(User user) {
+        DialogResult result = this.view.OpenForDeletion(user);
+        return result == DialogResult.OK ? user : null;
     }
 
     public User? GetById(int id, bool includeDeleted = false) {
